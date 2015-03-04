4bbefc9d2f84b6010a4ab97fc146e64336accb85
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-04 22:47:02 +0100

    Backported to support earlier version than PHP 5.4.

--- sample-files/upload/packages.php
+++ sample-files/upload/packages.php
@@ -4,26 +4,25 @@
 $files = glob("*.json");
 
 if ($files === false) {
-    $files = [];
+    $files = array();
 }
 
-$document = [
+$document = array(
     'minimumVersion' => 1,
-    'packages' => [],
-];
+    'packages' => array());
 
 foreach ($files as $file) {
     $data = json_decode(file_get_contents($file));
     if (isset($data->name) && isset($data->version)) {
-        $document['packages'][] = [
+        $document['packages'] = array(
             'name' => $data->name,
             'title' => isset($data->title) ? $data->title : $data->name,
             'version' => $data->version,
             'location' => basename($file),
-            'priority' => 1,
-        ];
+            'priority' => 1);
     }
 }
 
 header("Content-Type: text/plain");
 echo json_encode($document, JSON_PRETTY_PRINT);
+?>
