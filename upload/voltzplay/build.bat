a2156d84397b194ffc4acf1ef7b6e53bd72a25ba
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-09 15:19:42 +0100

    Updated mods.

--- upload/voltzplay/build.bat
+++ upload/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.0-all.jar --version "201503061732" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.0-all.jar --version "201503091517" --input . --output upload --manifest-dest "upload/voltzplay.json" 
