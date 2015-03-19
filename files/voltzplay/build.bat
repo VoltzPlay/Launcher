5d2c62c513e2a30e70acaf85b894246d997455a7
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-20 00:34:10 +0100

    Updated build.bat.

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201503192226" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201503200007" --input . --output upload --manifest-dest "upload/voltzplay.json" 
