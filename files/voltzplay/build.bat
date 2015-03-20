99af8cabf20fca5d5b616f03fcbfb7b16ce41007
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-20 15:51:26 +0100

    Updated Minechem.

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201503200856" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201503201548" --input . --output upload --manifest-dest "upload/voltzplay.json" 
