d5a14d6a3895c70fd1fd3983fd8bc988f430dafa
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-25 00:41:08 +0100

    Fixed typo in modpack.json.

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201503242354" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201503250033" --input . --output upload --manifest-dest "upload/voltzplay.json" 
