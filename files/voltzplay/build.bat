d981c9af8d701eba56eb5a0185d8574f4e6a80ca
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-24 15:47:11 +0100

    Optional mods includes dependcies now.

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,7 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201503231015" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+SET @YYYY=%@DATE:~6,4%
+SET @MM=%@DATE:~0,2%
+SET @DD=%@DATE:~3,2%
+SET @HOUR=%@TIME:~0,2%
+SET @MINUTE=%@TIME:~3,2%
+
+java -jar launcher-builder-1.0.2-all.jar --version !@YYYY!!@MM!!@DD!!@HOUR!!@MINUTE! --input . --output upload --manifest-dest "upload/voltzplay.json" 
