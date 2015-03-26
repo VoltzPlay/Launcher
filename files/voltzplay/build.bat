0deba649ab0b944120fd53f0e74e91686b89313a
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-26 13:40:59 +0100

    Updated Applied Energistics 2 to version Beta 17.

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201503260923" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201503261335" --input . --output upload --manifest-dest "upload/voltzplay.json" 
