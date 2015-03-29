6d545c6ae55f4d7e46eb1b99513d618a638194ae
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-29 14:47:42 +0200

    Updated Forge.

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201503272324" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201503291446" --input . --output upload --manifest-dest "upload/voltzplay.json" 
