220299eb45e87393f0c558c9041ef998a9f39588
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-30 00:00:07 +0200

    Updated Galacticraft to version 3.0.11.306

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201503291446" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201503292359" --input . --output upload --manifest-dest "upload/voltzplay.json" 
