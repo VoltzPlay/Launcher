8742fa560aef90ad676d5d7d04c6684c5fac1a29
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-04-03 18:31:17 +0200

    Updated Mekanism to version 8.1.0.214

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201504030005" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201504031755" --input . --output upload --manifest-dest "upload/voltzplay.json" 
