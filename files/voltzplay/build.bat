db4aa72380ac45c6c79e16868ec1cf2ad5884e76
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-04-04 00:03:13 +0200

    Updated Applied Energistics to version beta 23

--- files/voltzplay/build.bat
+++ files/voltzplay/build.bat
@@ -1,1 +1,1 @@
-java -jar launcher-builder-1.0.2-all.jar --version "201504031755" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.2-all.jar --version "201504040002" --input . --output upload --manifest-dest "upload/voltzplay.json" 
